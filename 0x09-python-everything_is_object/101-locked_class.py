b1f07426250c0a082e807dea171fd3e893f15d59
Abualiy <akremmuktar332@gmail.com>
2023-01-03 02:05:33 -0800

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""This defines a locked class"""
+
+
 class LockedClass:
+    """
+    only instatiation of an attribute called frist_name allowed
+    """
+
     __slots__ = ["first_name"]
