6190b7f856a41c0e02aad17aa6527e98d28304ee
Abualiy <akremmuktar332@gmail.com>
2023-01-09 03:17:16 -0800

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Thos module inherits from list"""
+
+
+class MyList(list):
+    """This class inherted from list"""
+
+    def print_sorted(self):
+        """function that sort unsorted list"""
+        print (sorted(self))
