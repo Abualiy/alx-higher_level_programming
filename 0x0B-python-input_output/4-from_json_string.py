0595805df9ca5040f2572826e2ed7e830bca3842
Abualiy <akremmuktar332@gmail.com>
2023-01-10 02:37:46 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    function that returns an object (Python data structure)
+    represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """function that returns an object"""
+    return json.loads(my_str)
