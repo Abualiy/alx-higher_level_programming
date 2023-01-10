a230211d7f0b8e6a12a7bed7b6ddf4deca0e27a0
Abualiy <akremmuktar332@gmail.com>
2023-01-10 03:23:36 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,10 +6,12 @@ class Student:
     """class Student that defines a student by"""
 
 
-    def __init__(self, frist_name, last_name, age):
-        self.frist_name = frist_name
+    def __init__(self, first_name, last_name, age):
+        """ Initializing """
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """get dictionary"""
         return self.__dict__
