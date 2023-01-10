930f0a82e4bb35b75c64ec8b26f0ecc405832ed1
Abualiy <akremmuktar332@gmail.com>
2023-01-10 03:31:06 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class Student that defines a student by"""
+
+
+class Student:
+    """class Student that defines a student by"""
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializing """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """get dictionary"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(slf, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
